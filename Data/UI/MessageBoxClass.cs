5316e9df61d767b3ebdb5362bdad9976f9ea22b9
harindu wijesinghe <hariwijesinghe234@gmail.com>
2020-11-10 13:19:27 +0530

    ui build

---
+++ Data/UI/MessageBoxClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirShare
+{
+    public class MessageBoxClass
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string BgColor { get; set; } = "white";
+        public Dictionary<string, (string Color,Action Action)> Buttons { get; set; }
+        public MessageBoxClass(string _Title, string _Message)
+        {
+            Title = _Title;
+            Message = _Message;
+            Buttons = new Dictionary<string, (string Color, Action Action)>() { { "OK", ("info", null) } };
+        }
+    }
+    
+}
