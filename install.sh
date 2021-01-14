bb10f1e4cc8a98472c3643ad534d6bd82a4d4bd4
HasinduLanka <hlrathnayaka@gmail.com>
2021-01-14 18:50:07 +0530

    sh

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 chmod +x *.sh
-sudo apt install screen
+sudo apt install screen openssh-server 
+
 
 mkdir sand
 cd sand
+
+
 
 wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
@@ -13,3 +16,4 @@ sudo apt-get update; \
   sudo apt-get update && \
   sudo apt-get install -y dotnet-sdk-5.0
   
+
---
+++ scripts/install-ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ~/.ssh
+sudo service ssh start 
+echo -e "\n\n\n" | ssh-keygen 
+cat id_rsa.pub >> authorized_keys
+chmod 640 authorized_keys
+sudo service ssh restart
