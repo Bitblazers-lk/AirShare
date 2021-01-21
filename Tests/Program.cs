87c7e22e8ec23212645507e72e20b07417ce4180
HasinduLanka <hlrathnayaka@gmail.com>
2021-01-21 16:05:14 +0530

    Welcome to BitBlazers

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -16,6 +16,7 @@ namespace Tests
             FFmpeg.SetExecutablesPath(Environment.CurrentDirectory, ffmpegExeutableName: "FFmpeg");
             IConversionResult result = await Conversion.ExtractAudio(@"E:\Artists\2XO\2XO - Blame It On You (Official Music Video).mp4", @"D:\My Projects\C# Git\AirShare\AirShare\Tmp\2XO - Summer Love ft. Ricky Garcia.mp3").Start();
             
+            
             Console.ReadLine();
         }
     }
