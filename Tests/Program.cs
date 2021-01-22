700462f0bc4f969029fb03bf2e1eb73381edfabb
HasinduLanka <hlrathnayaka@gmail.com>
2021-01-22 18:10:16 +0530

    FFMPEG test .net 5.0

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 using Xabe.FFmpeg;
+using Xabe.FFmpeg.Downloader;
 
 namespace Tests
 {
@@ -13,11 +15,78 @@ namespace Tests
         }
         static async void ex()
         {
-            FFmpeg.SetExecutablesPath(Environment.CurrentDirectory, ffmpegExeutableName: "FFmpeg");
-            IConversionResult result = await Conversion.ExtractAudio(@"E:\Artists\2XO\2XO - Blame It On You (Official Music Video).mp4", @"D:\My Projects\C# Git\AirShare\AirShare\Tmp\2XO - Summer Love ft. Ricky Garcia.mp3").Start();
-            
-            
+            Directory.CreateDirectory("workspace/");
+            Environment.CurrentDirectory = "workspace/";
+
+            System.Console.WriteLine("Checking FFmpeg");
+
+            await AirFFmpeg.Init();
+
+            System.Console.WriteLine("FFmpeg OK");
+            System.Console.WriteLine(Path.GetFullPath(Environment.CurrentDirectory));
+
+
+
+            string file = @"vidusaviya.mp4";
+            if (File.Exists(file))
+            {
+                System.Console.WriteLine("File Exists OK");
+            }
+
+            await (await FFmpeg.Conversions.FromSnippet.Convert(file, "vidusaviya.webm")).Start();
+
+            System.Console.WriteLine("File Converted");
+
             Console.ReadLine();
         }
+
+
+
     }
+
+    public static class AirFFmpeg
+    {
+        public static bool IsLoaded = false;
+
+        public static async Task Init()
+        {
+            IsLoaded = false;
+
+            string directoryWithFFmpegAndFFprobe = Path.Combine(Environment.CurrentDirectory, "AirFFmpeg");
+            FFmpeg.SetExecutablesPath(directoryWithFFmpegAndFFprobe, "ffmpeg", "ffprobe");
+
+            if (File.Exists(Path.Combine(directoryWithFFmpegAndFFprobe, "ffmpeg")))
+            {
+                IsLoaded = true;
+
+                return;
+            }
+
+            await InstallFF();
+
+
+        }
+
+        public static async Task InstallFF()
+        {
+            IsLoaded = false;
+            try
+            {
+                System.Console.WriteLine("Downloading FFmpeg...");
+                await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official);
+                IsLoaded = true;
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }
+
 }
+
+
