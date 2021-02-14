563bbc93c0747824c0804788624a1e8978f6371e
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-14 18:30:51 +0530

    dotnet scripts

--- AirShare/install.sh
+++ AirShare/install.sh
@@ -3,17 +3,4 @@
 chmod +x *.sh
 sudo apt install screen openssh-server 
 
-
-mkdir sand
-cd sand
-
-
-
-wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-5.0
-  
-
+echo "You may install Dotnet from the shell files located in this directory"
