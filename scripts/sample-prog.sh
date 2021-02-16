2bdea567ee2a4e72a30d626adb51f41b323b6375
HasinduLanka <hlrathnayaka@gmail.com>
2021-02-16 17:32:36 +0530

    sampleprog

--- scripts/sample-prog.sh
+++ scripts/sample-prog.sh
@@ -2,3 +2,10 @@
 
 echo -e "Hi $USER"
 echo "I am the sample output"
+
+
+spscr="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+sp="$(dirname "$spscr")"
+
+echo Current Application Directory is at 
+echo $sp
